10e5ba182dffbd482d6c3328cc2eb7138ea9b488
Mikaul <mikaulmcdermid@gmail.com>
2023-12-05 23:44:19 -0600

    forgot py files

--- classification_project/prepare.py
+++ classification_project/prepare.py
@@ -4,14 +4,14 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 def prep_iris(df):
     """
-    write meaningful docstrings
+    Prep iris function takes in the dataframe and adjusts it via dropping columns "species_id" and "measurement_id" and renaming the column "species_name" to "species". Once cleaned, the dataframe is returned.
     """
     df = df.drop(columns=['species_id','measurement_id'])
     df = df.rename(columns={"species_name":'species'})
     return df
 def clean_titanic(df):
     """
-    students - write docstring
+    Clean titanic function takes in the dataframe and adjusts it via dropping columns "embarked", "age", "deck" and "class". It changes the column "pclass" to an object dtype and finally adjusts the "embark_town" column by filling invalid entry types with the mode of the column. Once cleaned, the dataframe is returned and can be renamed for further analysis.
     """
     #drop unncessary columns
     df = df.drop(columns=['embarked', 'age','deck', 'class'])
@@ -22,14 +22,14 @@ def clean_titanic(df):
     return df
 def prep_telco(df):
     '''
-    please write good docstrings
+    Prep telco function takes in the dataframe and adjusts it via dropping columns "payment_type_id", "internet_service_type_id", and "contract_type_id". It accounts for an issue in the "total_charges" column by adjusting a space value and converting it to a float so that the dtype can be adjusted. Once cleaned, the dataframe is returned and can be renamed for further analysis. 
     '''
     df = df.drop(columns = ['payment_type_id','internet_service_type_id','contract_type_id'])
     df.total_charges = df.total_charges.str.replace(' ', '0.0')
     return df
 def splitting_data(df, col):
     '''
-    i will write a docstring, like send in target variable
+    Splitting data function takes in two arguments, one dataframe and the column you want to stratify on. After receiving these two arguments, the first split occurs and splits the data 60/40 for the train portion of the returns. A second split is performed on the remaining 40% of the data at a 50/50 ratio to make sure the validate and test returns are equal size. Random state of 24 because Kobe. Returns all 3 data sets.
     '''
     #first split
     train, validate_test = train_test_split(df,
@@ -56,39 +56,11 @@ def preprocess_telco(telco_train, telco_val, telco_test):
     columns sex and embark_town encoded in the one-hot fashion
     return: (pd.DataFrame, pd.DataFrame, pd.DataFrame)
     '''
-    # with a looping structure:
-    # for df in [train_df, val_df, test_df]:
-    #     df.drop(blah blah blah)
-    #     df['pclass'] = df['pclass'].astype(int)
-    
-#     yesno_columns = ['paperless_billing', 'streaming_tv', 'streaming_movies', 'tech_support']
-    
-#     # Loop through each dataframe
-#     for df in [telco_train, telco_val, telco_test]:
-#         # Loop through each column that needs conversion
-#         for col in yesno_columns:
-#             # Replace 'Yes' with 1 and 'No' with 0 in the current column
-#             df[col] = df[col].replace({'Yes': 1, 'No': 0})
-    
-#    telco_train = telco_train.drop(columns='customer_id')
+   
     telco_train['monthly_charges'] = telco_train['monthly_charges'].astype(int)
-#     telco_train['streaming_tv'] = telco_train['streaming_tv'].astype(int)
-#     telco_train['streaming_movies'] = telco_train['streaming_movies'].astype(int)
-#     telco_train['tech_support'] = telco_train['tech_support'].astype(int)
-    
-#    telco_val = telco_val.drop(columns='customer_id')
     telco_val['monthly_charges'] = telco_val['monthly_charges'].astype(int)
-#     telco_val['streaming_tv'] = telco_val['streaming_tv'].astype(int)
-#     telco_val['streaming_movies'] = telco_val['streaming_movies'].astype(int)
-#     telco_val['tech_support'] = telco_val['tech_support'].astype(int)  
-    
-#    test_df = telco_test.drop(columns='customer_id')
     telco_test['monthly_charges'] = telco_test['monthly_charges'].astype(int)
-#     telco_test['streaming_tv'] = telco_test['streaming_tv'].astype(int)
-#     telco_test['streaming_movies'] = telco_test['streaming_movies'].astype(int)
-#     telco_test['tech_support'] = telco_test['tech_support'].astype(int)
-    
-    
+
     
     encoding_var = ['gender', 'partner', 'dependents', 'phone_service', 'multiple_lines', 'online_security', 'online_backup', 'device_protection', 'tech_support', 'streaming_tv', 'streaming_movies', 'paperless_billing', 'churn', 'contract_type', 'internet_service_type', 'payment_type']
     
